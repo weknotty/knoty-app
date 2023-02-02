0abbaf425a32b5a2c136a259c7bbf96f904ee882
amitedri <amitedri778@gmail.com>
2023-02-02 11:25:24 +0200

    updates

--- src/Components/Welcome/index.js
+++ src/Components/Welcome/index.js
@@ -1,7 +1,7 @@
 
 const Welcome = () => {
   return (
-    <div className="col-12 d-flex flex-column justify-content-center align-items-center bg-dark fullHeight">
+    <div className="col-12 d-flex flex-column justify-content-center align-items-center imgBg fullHeight animated">
       <img src="/assets/icons/logo.svg" height="300" width="300" />
     </div>
   );
